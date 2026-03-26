c8917e8c7f54f4520038b37a93b6cbf1d9abc854
AHuq0127 <ahuq2@calstatela.edu>
2026-03-26 05:35:03 -0700

    finished history.js

---
+++ history.js
@@ -0,0 +1,43 @@
+import inquirer from 'inquirer';
+import { readFileSync } from 'fs';
+import { searchFlow } from './app.js';
+
+const historyCommand = async (arg) => {
+    if (arg !== 'keywords') {
+        console.log('Invalid argument. Use: node cli.js history keywords');
+        return;
+    }
+
+    let history = [];
+
+    try {
+        const data = readFileSync('./search_history.json', 'utf-8');
+        history = JSON.parse(data);
+    } catch (error) {
+        console.log('No search history found.');
+        return;
+    }
+
+    if (history.length === 0) {
+        console.log('No search history found.');
+        return;
+    }
+
+    const answer = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'keyword',
+            message: 'Select a keyword:',
+            choices: ['Exit', ...history]
+        }
+    ]);
+
+    if (answer.keyword === 'Exit') {
+        console.log('Exiting.');
+        return;
+    }
+
+    await searchFlow(answer.keyword);
+};
+
+export { historyCommand };
